dac8fe17a048847106216157de75911ac6548787
Moamin <moamin.salamah@gmail.com>
2023-02-13 10:02:16 +0100

    project setup

---
+++ src/components/NavBar.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-app>
+    <div>Navbar</div>
+  </v-app>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app>
+    <NavBar />
+    <v-container fluid></v-container>
+  </v-app>
+</template>
+
+<script>
+import NavBar from "@/components/NavBar.vue";
+export default {
+  name: "HomeView",
+
+  components: {
+    NavBar,
+  },
+};
+</script>
